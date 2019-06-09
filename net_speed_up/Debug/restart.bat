a6cc67f2a1f989a846d3cb9644ce1901ef2597db
1030225735 <1030225735@qq.com>
2019-06-09 20:15:46 +0800

    commit

---
+++ net_speed_up/Debug/restart.bat
@@ -0,0 +1,4 @@
+taskkill /fi  'windowtitle eq net-speed-up'
+taskkill /f /im  'net_speed_up.exe'
+start F:\\vs\\net_speed_up\\Debug\\start.bat
+exit
---
+++ net_speed_up/Debug/start.bat
@@ -0,0 +1,3 @@
+taskkill /f /fi "IMAGENAME eq net_speed_up*"
+start F:\\vs\\net_speed_up\\Debug\\net_speed_up.exe
+exit
